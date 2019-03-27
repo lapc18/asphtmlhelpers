694e2b5fe0decd6f2c49d6bf97d4fa2844cd922d
lapc18 <lapc18@outlook.com>
2019-03-26 21:24:26 -0400

    Modelo con validaciones ready

---
+++ HTMLHelpers/Models/FormularioModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HTMLHelpers.Models
+{
+    public class FormularioModel
+    {
+        [Required(ErrorMessage = "Este campo es oblogario.")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Este campo es oblogario.")]
+
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "Este campo es oblogario.")]
+        public string Cedula { get; set; }
+
+        [Required]
+        [Range(18,99, ErrorMessage = "Debe ingresar una edad verdadera.")]
+        public int Edad { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string Telefono { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligario.")]
+        public string Genero { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligario.")]
+        public string EstadoCivil { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligario.")]
+        public string Hobbys { get; set; }
+
+    }
+}
